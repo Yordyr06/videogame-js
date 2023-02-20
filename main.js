2f4671472a7bbdf7bac25af1147aa1ad880903b1
Yordy <yoalmonte.dev@outlook.com>
2023-02-20 05:50:33 -0400

    Tercera clase: comprendiendo las propiedad de canvas. Incorporando elementos al canvas. Mas sobre canvas

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
+let canvasSize;
+
 window.addEventListener('load', startGame);
 
 function startGame() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  } 
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  const elementsSize = canvasSize / 10;
   
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+  };
 }
