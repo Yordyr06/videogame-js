344683442cd5848bd9de931f8878fcbfbab60025
Yordy <yoalmonte.dev@outlook.com>
2023-02-24 03:47:41 -0400

    Octava clase: evento keydown. Creacion de funciones de movimientos

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
+let btnUp = document.getElementById('up');
+let btnLeft = document.getElementById('left');
+let btnRight = document.getElementById('right');
+let btnDown = document.getElementById('down');
+
 let canvasSize;
 let elementsSize;
 
+
+
+
 window.addEventListener('load', startGame);
 window.addEventListener('resize', setCanvasSize);
+
+window.addEventListener('keydown', move);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
@@ -21,6 +37,8 @@ function setCanvasSize() {
   startGame();
 }
 
+
+
 function startGame() {
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
@@ -29,10 +47,36 @@ function startGame() {
   const mapRows = map.trim().split('\n');
   const mapCols = mapRows.map(row => row.trim().split(''));
 
-  for (let row = 1; row <= 10; row++) {
-    for (let col = 1; col <= 10; col++) {
-      game.fillText(emojis[mapCols[row-1][col-1]], elementsSize * col, elementsSize * row);
-    };
-  };
+  mapCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const x = elementsSize * (colI + 1);
+      const y = elementsSize * (rowI + 1);
+      game.fillText(emoji, x, y);
+    });
+  });
+};
+
+
+function move(event) {
+  if (event.key == 'ArrowUp') moveUp();
+  else if (event.key == 'ArrowLeft') moveLeft();
+  else if (event.key == 'ArrowRight') moveRight();
+  else if (event.key == 'ArrowDown') moveDown();
 }
 
+function moveUp() {
+  console.log('Me quiero jondia parriba');
+}
+
+function moveLeft() { 
+  console.log('Me quiero jondia pa la iquielda');
+}
+
+function moveRight() {
+  console.log('Me quiero jondia pa la derecha');
+}
+
+function moveDown() {
+  console.log('Me quiero jondia pabajo');
+}
